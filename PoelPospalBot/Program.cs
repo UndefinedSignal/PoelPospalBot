f96abcbd5c6e71ec2883a97d46a4ea8106687f41
TestUnit <testunit@mail.ru>
2022-02-01 23:00:22 +0700

    Initialization

--- PoelPospalBot/Program.cs
+++ PoelPospalBot/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 
 namespace PoelPospalBot
 {
     class Program
     {
-        static void Main(string[] args)
+        private DiscordSocketClient _client;
+
+        public static Task Main(string[] args) => new Program().Start();
+
+        private async Task Start()
         {
-            Console.WriteLine("Hello World!");
+            _client = new DiscordSocketClient();
+
+            _client.Log += Logger;
+
+            var token = LoadToken();
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Logger(LogMessage msg)
+        {
+            Console.WriteLine(msg);
+            return Task.CompletedTask;
+        }
+
+        private string LoadToken()
+        {
+            return System.IO.File.ReadAllText(@"..\..\..\Config\token.txt");
         }
     }
 }
