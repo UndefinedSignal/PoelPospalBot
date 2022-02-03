69cd079f9ff0b58b7827dfa94eae845088d0a361
TestUnit <nod33@inbox.ru>
2022-02-04 02:42:36 +0700

    Startup rework

--- PoelPospalBot/Program.cs
+++ PoelPospalBot/Program.cs
@@ -7,32 +7,7 @@ namespace PoelPospalBot
 {
     class Program
     {
-        private DiscordSocketClient _client;
-
-        public static Task Main(string[] args) => new Program().Start();
-
-        private async Task Start()
-        {
-            _client = new DiscordSocketClient();
-
-            _client.Log += Logger;
-
-            var token = LoadToken();
-            await _client.LoginAsync(TokenType.Bot, token);
-            await _client.StartAsync();
-
-            await Task.Delay(-1);
-        }
-
-        private Task Logger(LogMessage msg)
-        {
-            Console.WriteLine(msg);
-            return Task.CompletedTask;
-        }
-
-        private string LoadToken()
-        {
-            return System.IO.File.ReadAllText(@"..\..\..\Config\token.txt");
-        }
+        public static Task Main(string[] args)
+            => Startup.RunAsync(args);
     }
 }
---
+++ PoelPospalBot/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PoelPospalBot
+{
+    class Startup
+    {
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory).AddJsonFile("_config.json");
+            Configuration = builder.Build();
+        }
+
+        public static async Task RunAsync(string[] args)
+        {
+            var Startup = new Startup(args);
+            await Startup.RunAsync();
+        }
+
+        public async Task RunAsync()
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+
+            var provider = services.BuildServiceProvider();
+
+            //await provider.GetRequiredService<StartupService>().StartAsync(); TODO
+            await Task.Delay(-1);
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Verbose,
+                MessageCacheSize = 1000
+            }))
+                .AddSingleton(new CommandService(new CommandServiceConfig
+                {
+                    LogLevel = LogSeverity.Verbose,
+                    DefaultRunMode = RunMode.Async,
+                }));
+        }
+    }
+}
